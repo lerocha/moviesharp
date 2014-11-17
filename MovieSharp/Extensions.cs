110d85086dc804f1d976361e695deac705a58875
Luis Rocha <lerocha@gmail.com>
2014-11-16 18:34:08 -0800

    removing unused code

--- MovieSharp/Extensions.cs
+++ MovieSharp/Extensions.cs
@@ -9,24 +9,6 @@ namespace MovieSharp
 	public static class Extensions
 	{
 		/// <summary>
-		/// Extends Type class with IsAnonymous method:
-		/// http://www.liensberger.it/web/blog/?p=191
-		/// </summary>
-		/// <param name="type">The type.</param>
-		/// <returns>
-		///   <c>true</c> if the specified type is anonymous; otherwise, <c>false</c>.
-		/// </returns>
-		public static bool IsAnonymous(this Type type)
-		{
-			if (type == null)
-				return false;
-			return Attribute.IsDefined(type, typeof(CompilerGeneratedAttribute), false)
-			&& type.IsGenericType && type.Name.Contains("AnonymousType")
-			&& (type.Name.StartsWith("<>") || type.Name.StartsWith("VB$"))
-			&& (type.Attributes & TypeAttributes.NotPublic) == TypeAttributes.NotPublic;
-		}
-
-		/// <summary>
 		/// Serialize this object into JSON.
 		/// </summary>
 		/// <param name="obj"></param>
