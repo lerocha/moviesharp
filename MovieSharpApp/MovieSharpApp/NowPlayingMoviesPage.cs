0530c8f4d6091466fc8aaca9d207b38b873d0e7a
Luis Rocha <lerocha@gmail.com>
2014-11-16 21:15:42 -0800

    Adding a sample app using MovieSharp

---
+++ MovieSharpApp/MovieSharpApp/NowPlayingMoviesPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using MovieSharp.Data;
+using System.Diagnostics;
+using MovieSharp;
+
+namespace MovieSharpApp
+{
+	public class NowPlayingMoviesPage : ContentPage
+	{
+		private ListView listView;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MovieSharpApp.NowPlayingMoviesPage"/> class.
+		/// </summary>
+		public NowPlayingMoviesPage()
+		{
+			Title = "Now Playing";
+
+			listView = new ListView {
+				RowHeight = 80
+			};
+
+			Content = new StackLayout {
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				Children = { listView }
+			};
+		}
+
+		/// <summary>
+		/// Raises the appearing event.
+		/// </summary>
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+
+			try {
+				// TODO: replace with the real API key
+				IMovieSharpClient movieSharpClient = new MovieSharpClient("_YOUR_API_KEY_");
+
+				var response = await movieSharpClient.GetNowPlayingMoviesAsync();
+				if (response.IsOk) {
+					List<string> movies = new List<String>();
+					foreach(Movie movie in response.Body.Results) {
+						movies.Add(movie.Title);
+					}
+					listView.ItemsSource = movies;
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+				Debug.WriteLine(e);
+			}
+		}
+	}
+}
+
