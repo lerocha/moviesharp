6c11f6532c7394a57fc4e790631d2bfadccbdeae
Luis Rocha <lerocha@gmail.com>
2014-11-22 10:32:37 -0800

    cleaning up test base class

--- MovieSharp.Test/AbstractMovieSharpClientTest.cs
+++ MovieSharp.Test/AbstractMovieSharpClientTest.cs
@@ -5,9 +5,6 @@ namespace MovieSharp.Test
     public abstract class AbstractMovieSharpClientTest
     {
         protected readonly string ApiKey = ConfigurationManager.AppSettings["ApiKey"];
-        protected readonly string ConsumerKey = ConfigurationManager.AppSettings["ConsumerKey"];
-        protected readonly string ConsumerSecret = ConfigurationManager.AppSettings["ConsumerSecret"];
-        protected readonly string RefreshToken = ConfigurationManager.AppSettings["RefreshToken"];
         protected const string ContactId = "003i000000W2RMDAA3";
         protected const long MovieId = 238;
     }
